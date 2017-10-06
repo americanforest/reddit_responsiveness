56d74443eacf93c7fa6720a294e7f89d617c4184
Alex <alexl@tcsatl.org>
2017-10-06 13:56:48 -0400

    More work on JS script

--- js/redditjson.js
+++ js/redditjson.js
@@ -1,16 +1,22 @@
-$(function () {
-    $('#domainform').on('submit', function (event) { //Upon submission of the domainform form
+$(function() {
+    $('#domainform').on('submit', function(event) { //Upon submission of the domainform form
+        
         event.preventDefault(); //Prevent reverting to default
-      
+
         var subreddit = $('#s').val(); //Get user entered subreddit
-        
+
         var cors_proxy = "https://cors.now.sh/"; //CORS proxy
-        
-        var requrl = "http://www.reddit.com/r/"+subreddit+"/about.json"; //Reddit API address
-        
-        var fullurl = cors_proxy + requrl;
-        
-        console.log(fullurl);
-        
-    }); 
+
+        var requrl = "http://www.reddit.com/r/" + subreddit + "/new.json?limit=5"; //Reddit API address
+
+        var fullurl = cors_proxy + requrl; //Combine CORS Proxy with request REST URL for full URL
+
+        $.getJSON(fullurl, function(jsonoutput) { //Make JSON request with full URL
+
+            threads = jsonoutput.data.children;
+
+            console.log(threads[1]["data"]["created_utc"]);
+
+        });
+    });
 });
